10fc09ebba99cc56e612a60e336352c6c0747069
ilya Morozov <ilya.mister.2010@gmail.com>
2023-11-21 17:19:11 +0300

    added scene ar

---
+++ lib/widgets/navigations.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+// Views
+import '../views/home-view.dart';
+import '../views/ar-scene-view.dart';
+
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Navigation(),
+    );
+  }
+}
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.view_in_ar)),
+            label: 'Scene AR',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        const HomeView(title: 'Flutter AR Page'),
+        const ArSceneView()
+      ][currentPageIndex],
+    );
+  }
+}

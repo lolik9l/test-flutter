9349f023865577be404cf51107a9e8c59084d2c3
ilya Morozov <ilya.mister.2010@gmail.com>
2023-11-21 20:10:04 +0300

    try other plugin

--- lib/views/ar-scene-view.dart
+++ lib/views/ar-scene-view.dart
@@ -1,117 +1,89 @@
-import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
-import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
-import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ArSceneView extends StatefulWidget {
   const ArSceneView({super.key});
+
   @override
   _ArSceneViewState createState() => _ArSceneViewState();
 }
 
 class _ArSceneViewState extends State<ArSceneView> {
-  ARSessionManager? arSessionManager;
-  ARObjectManager? arObjectManager;
-  bool _showFeaturePoints = false;
-  bool _showPlanes = false;
-  bool _showWorldOrigin = false;
-  final bool _showAnimatedGuide = true;
-  final String _planeTexturePath = "../assets/images/triangle.png";
-  final bool _handleTaps = false;
-
-  @override
-  void dispose() {
-    super.dispose();
-    arSessionManager!.dispose();
-  }
+  late ArCoreController arCoreController;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-          title: const Text('Debug Options'),
+          title: const Text('Hello World'),
         ),
-        body: Container(
-            child: Stack(children: [
-          ARView(
-            onARViewCreated: onARViewCreated,
-            planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
-            showPlatformType: true,
-          ),
-          Align(
-            alignment: FractionalOffset.bottomRight,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              color: const Color(0x0fffffff).withOpacity(0.5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SwitchListTile(
-                    title: const Text('Feature Points'),
-                    value: _showFeaturePoints,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _showFeaturePoints = value;
-                        updateSessionSettings();
-                      });
-                    },
-                  ),
-                  SwitchListTile(
-                    title: const Text('Planes'),
-                    value: _showPlanes,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _showPlanes = value;
-                        updateSessionSettings();
-                      });
-                    },
-                  ),
-                  SwitchListTile(
-                    title: const Text('World Origin'),
-                    value: _showWorldOrigin,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _showWorldOrigin = value;
-                        updateSessionSettings();
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ])));
-  }
-
-  void onARViewCreated(
-      ARSessionManager arSessionManager,
-      ARObjectManager arObjectManager,
-      ARAnchorManager arAnchorManager,
-      ARLocationManager arLocationManager) {
-    this.arSessionManager = arSessionManager;
-    this.arObjectManager = arObjectManager;
-
-    this.arSessionManager!.onInitialize(
-          showFeaturePoints: _showFeaturePoints,
-          showPlanes: _showPlanes,
-          customPlaneTexturePath: _planeTexturePath,
-          showWorldOrigin: _showWorldOrigin,
-          showAnimatedGuide: _showAnimatedGuide,
-          handleTaps: _handleTaps,
-        );
-    this.arObjectManager!.onInitialize();
-  }
-
-  void updateSessionSettings() {
-    arSessionManager!.onInitialize(
-      showFeaturePoints: _showFeaturePoints,
-      showPlanes: _showPlanes,
-      customPlaneTexturePath: _planeTexturePath,
-      showWorldOrigin: _showWorldOrigin,
+        body: ArCoreView(
+          onArCoreViewCreated: _onArCoreViewCreated,
+        ),
+      ),
     );
+  }
+
+  void _onArCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
+
+    _addSphere(arCoreController);
+    _addCylindre(arCoreController);
+    _addCube(arCoreController);
+  }
+
+  void _addSphere(ArCoreController controller) {
+    final material =
+        ArCoreMaterial(color: const Color.fromARGB(120, 66, 134, 244));
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(0, 0, -1.5),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addCylindre(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Colors.red,
+      reflectance: 1.0,
+    );
+    final cylindre = ArCoreCylinder(
+      materials: [material],
+      radius: 0.5,
+      height: 0.3,
+    );
+    final node = ArCoreNode(
+      shape: cylindre,
+      position: vector.Vector3(0.0, -0.5, -2.0),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addCube(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: const Color.fromARGB(120, 66, 134, 244),
+      metallic: 1.0,
+    );
+    final cube = ArCoreCube(
+      materials: [material],
+      size: vector.Vector3(0.5, 0.5, 0.5),
+    );
+    final node = ArCoreNode(
+      shape: cube,
+      position: vector.Vector3(-0.5, 0.5, -3.5),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
   }
 }
